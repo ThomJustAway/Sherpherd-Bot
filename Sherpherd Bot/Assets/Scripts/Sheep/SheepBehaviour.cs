57fba3523318e04715dfb2919da568c04f7e7022
Thom Low <thomthomlow@gmail.com>
2024-04-19 21:44:03 +0800

    Init

---
+++ Sherpherd Bot/Assets/Scripts/Sheep/SheepBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Sheep
+{
+    public class SheepBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float walkingState;
+        [SerializeField] private float rotationSpeed;
+
+
+    }
+
+    public enum SheepStates
+    {
+        Drink,
+        Eat,
+        Sleep,
+        Idle,
+        GrowFur,
+        Movement
+    }
+}
---
+++ Sherpherd Bot/Assets/Scripts/Sheep/State/SheepMovementState.cs
@@ -0,0 +1,24 @@
+﻿using PGGE.Patterns;
+using System.Collections;
+using UnityEngine;
+
+namespace Sheep
+{
+    public class SheepState : FSMState
+    {
+        private SheepBehaviour sheep;
+
+        public SheepState(FSM fsm , SheepBehaviour sheep) : base(fsm)
+        {
+            this.sheep = sheep;
+        }
+    }
+
+    public class SheepMovementState : SheepState
+    {
+        public SheepMovementState(FSM fsm, SheepBehaviour sheep) : base(fsm, sheep)
+        {
+            ID = (int) SheepStates.Movement;
+        }
+    }
+}
